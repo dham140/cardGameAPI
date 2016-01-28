646f9db6d9e72a845640a261c9141659a333841e
Evan Arnold <evana@slalom.com>
2016-01-28 08:37:18 -0800

    add a way to access the yodatalk site via SOAP rather than scraping

--- src/main/scala/ScalatraBootstrap.scala
+++ src/main/scala/ScalatraBootstrap.scala
@@ -1,9 +1,26 @@
+import _root_.akka.actor.{Props, ActorSystem}
 import com.example.app._
 import org.scalatra._
 import javax.servlet.ServletContext
 
 class ScalatraBootstrap extends LifeCycle {
+//  override def init(context: ServletContext) {
+//    context.mount(new TestServlet, "/*")
+//  }
+
+  // Get a handle to an ActorSystem and a reference to one of your actors
+  val system = ActorSystem()
+//  val myActor = system.actorOf(Props[/*DO STUFF*/])
+
+  // In the init method, mount your servlets with references to the system
+  // and/or ActorRefs, as necessary.
   override def init(context: ServletContext) {
-    context.mount(new TestServlet, "/*")
+    context.mount(new TestServlet(system), "/*")
+//    context.mount(new MyActorApp(system, myActor), "/actors/*")
+  }
+
+  // Make sure you shut down
+  override def destroy(context:ServletContext) {
+    system.terminate()
   }
 }
--- src/main/scala/com/example/app/TestServlet.scala
+++ src/main/scala/com/example/app/TestServlet.scala
@@ -1,11 +1,20 @@
 package com.example.app
 
 import java.net.URLEncoder
+import javax.xml.ws.BindingProvider
+import _root_.akka.actor.{ActorSystem, Actor}
 import org.scalatra._
 import scalate.ScalateSupport
 import dispatch._, Defaults._
+import yodawsdl.YodaService
+import scala.concurrent.ExecutionContext
 
-class TestServlet extends CameoserviceStack {
+class TestServlet(system: ActorSystem) extends CameoserviceStack with FutureSupport {
+
+  val dialects = Map(
+    "pirate" -> {PirateTranslator.translate(_)},
+    "morsecode" -> {MorseCodeTranslator.translate(_)}
+  )
 
   post("/translate/:dialect/?") {
 
@@ -16,6 +25,29 @@ class TestServlet extends CameoserviceStack {
       case "morsecode" => MorseCodeTranslator.translate(request.body)
       case _ => "FAILED UNKNOWN DIALECT " + dialect
     }
+
+    //Alternate selection mechanism
+//    dialects.getOrElse(dialect, s"Unknown dialect: $dialect. Please select from ${dialects.keys.mkString(", ")}")
+
+
   }
 
+  get("/") {
+    new AsyncResult() {
+      override val is =
+        Future {
+          // Add async logic here
+          <html><body>Hello Akka</body></html>
+        }
+    }
+
+//    val a = new YodaService()
+//    a.getYodatalkPort match {
+//      case bp: BindingProvider =>
+//        bp.yodaTalk("This is a test message")
+//      case _ => "FAILURE"
+//    }
+  }
+
+  override protected implicit def executor: ExecutionContext = scala.concurrent.ExecutionContext.Implicits.global
 }
