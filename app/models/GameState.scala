5af634461c864f051b9502efef6657d59c095d74
Daniel Hamilton <daniel.hamilton@slalom.com>
2016-02-10 11:21:00 -0800

    Made GameState a case class Added gameStateHistory to keep track of GameState instances

--- app/models/GameState.scala
+++ app/models/GameState.scala
@@ -5,11 +5,6 @@ import scala.util.Random
 /**
  * Created by evana on 2/8/16.
  */
-class GameState {
-  var decks: Seq[Seq[Card]] = Random.shuffle((1 to 13).map(Card(_, "♠︎")) ++
-    (1 to 13).map(Card(_, "♣")) ++
-    (1 to 13).map(Card(_, "♥")) ++
-    (1 to 13).map(Card(_, "♦"))).sliding(26,26).toSeq
+case class GameState(decks: Seq[Seq[Card]], comparePile: Seq[Card])
 
-  var comparePile = Seq[Card]()
-}
+
